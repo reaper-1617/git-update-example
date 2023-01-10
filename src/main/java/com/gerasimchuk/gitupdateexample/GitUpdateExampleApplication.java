54858a506edcd4d4b2174612f31e9f24400be1d7
reaper-1617 <com.gerasimchuk.maksim@gmail.com>
2023-01-10 14:17:11 +0300

    #init commit

---
+++ src/main/java/com/gerasimchuk/gitupdateexample/GitUpdateExampleApplication.java
@@ -0,0 +1,13 @@
+package com.gerasimchuk.gitupdateexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitUpdateExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GitUpdateExampleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/gerasimchuk/gitupdateexample/GitUpdateExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerasimchuk.gitupdateexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitUpdateExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
